ca067dc008d4343dedb5509011c68401b419b198
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-09-10 19:34:07 -0300

    feat: add deploy scripts

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return redirect('/app');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
